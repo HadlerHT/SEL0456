061ed63053c826efc29c72f7dc1dcb4812a4419c
HadlerHT <hadlerht7@gmail.com>
2023-09-22 16:17:27 -0300

    Parte 1 trabalho

--- Projeto1/main.c
+++ Projeto1/main.c
@@ -15,14 +15,14 @@ double cube (double x) {
 
 int main (void) {
 
-	double entry;
+	double entry[] = {2.2, 4.6, 5.5, 7.8, 23.1};
 
-	printf ("Type in a number: ");
-	scanf ("%lf", &entry);
+	unsigned k;
+	for (k = 0; k < 5; k++) {
+		printf ("The half of %.3lf equals %.3lf\n", entry[k], half(entry[k]));
+		printf ("The square of %.3lf equals %.3lf\n", entry[k], square(entry[k]));
+		printf ("The cube of %.3lf equals %.3lf\n\n", entry[k], cube(entry[k]));
+	}
 	
-	printf ("The half of %.3lf equals %.3lf\n", entry, half(entry));
-	printf ("The square of %.3lf equals %.3lf\n", entry, square(entry));
-	printf ("The cube of %.3lf equals %.3lf\n", entry, cube(entry));
-		
 	return 0;
 }
